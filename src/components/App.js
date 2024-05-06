47515f6f5a6078e845c1f4ad4e96c4a57c3ffc1f
Orian shillz <91493524+orian3737@users.noreply.github.com>
2024-05-06 11:58:29 -0400

    pass fail

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-import React from "react";
-import blogData from "../data/blog";
+import React from 'react';
+import Header from './Header';
+import About from './About';
+import ArticleList from './ArticleList';
+import { blogData, posts } from '../../data/blog'; 
 
-console.log(blogData);
 
 function App() {
-  return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header name={blogData.name} />
+            <About image={blogData.image} about={blogData.about} />
+            <ArticleList posts={posts} />
+        </div>
+    );
 }
 
 export default App;
